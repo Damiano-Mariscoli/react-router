ccf2da7886ffaed05662e78dd1c562e80121e962
Damiano-Mariscoli <damiano.mariscoli2@gmail.com>
2024-12-06 14:13:17 +0100

    ADDED: route per la home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,18 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { languages } from "./data/languages";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./HomePage.jsx";
 
-function App() {}
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
